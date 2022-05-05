534c590cebd036e2d93d1e8eca8fc42c9f3e97bc
Chan0515 <32955432+Chan0515@users.noreply.github.com>
2022-05-05 13:10:22 -0700

    Updated App link URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import NBA from './NBA';
 import Home from './Home';
 function App() {
     return(
         <div>
             <Navbar/>
-            <Routes>
-                <Route index element = {<Home/>} path = "/"></Route>
-                <Route element = {<NBA/>} path = "/NBA"></Route>
-            </Routes>
+            <Home/>
         </div>
     )
 }
--- frontend/src/components/AppDisplay/AppsList.js
+++ frontend/src/components/AppDisplay/AppsList.js
@@ -1,7 +1,7 @@
 export const AppsList = [
     {
         title: "NBA Fantasy Advanced Stats Table",
-        github: "https://github.com/Chan0515/NBA-Fantasy-App",
+        github: "https://chan0515.github.io/NBA-Fantasy-App/",
         appLink: 'NBA',
         appIcon: "fa-solid fa-basketball",
         appDesc: "A dynamic table that analyzes player value based on deviation from the league average. Data scraped from basketball- reference.com and fed to PostGresQL database with a Python script every 24 hours.",
